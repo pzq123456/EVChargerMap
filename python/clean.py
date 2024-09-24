c6833bb0637a64217a58a45fa80f82f85fdf1112
PanZhiQing <panzhiqing15@gmail.com>
2024-09-24 15:20:16 +0800

    Refactor marker cluster module and add utility functions

--- python/clean.py
+++ python/clean.py
@@ -25,7 +25,21 @@ def count_data():
     city_count.to_csv("data/city_count.csv")
     state_count.to_csv("data/state_count.csv")
 
+# 1	阿拉巴马州	Alabama	AL
+# 2	阿拉斯加州	Alaska	AK
+# 3	亚利桑那州	Arizona	AZ
+def clean_state():
+    # 将 usa.txt 中的数据转换为 csv 只保留英文名和对应的缩写
+    with open("data/usa.txt", "r") as f:
+        data = f.readlines()
+        data = [x.split("\t") for x in data]
+        data = [[x[2], x[3]] for x in data]
+        df = pd.DataFrame(data, columns=["State", "Abbreviation"])
+        df.to_csv("data/usa.csv", index=False)
+
+
 if __name__ == "__main__":
     # clean_data()
     # print("Data cleaned and saved to", SAVE_PATH)
-    count_data()
+    # count_data()
+    clean_state()
